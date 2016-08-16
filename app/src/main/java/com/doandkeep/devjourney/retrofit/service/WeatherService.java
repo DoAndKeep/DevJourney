a53c6eef3551fb868fd9c1c10c05e77be577232b
tao.zhang <tao.zhang@dmall.com>
2016-08-16 14:39:49 +0800

    添加Retrofit的Header使用

--- app/src/main/java/com/doandkeep/devjourney/retrofit/service/WeatherService.java
+++ app/src/main/java/com/doandkeep/devjourney/retrofit/service/WeatherService.java
@@ -2,8 +2,12 @@ package com.doandkeep.devjourney.retrofit.service;
 
 import com.doandkeep.devjourney.bean.weather.WeatherWrapper;
 
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.HeaderMap;
 import retrofit2.http.Headers;
 import retrofit2.http.Query;
 import retrofit2.http.Url;
@@ -15,4 +19,11 @@ public interface WeatherService {
     @Headers("apikey:b86c2269fe6588bbe3b41924bb2f2da2")
     @GET
     Call<WeatherWrapper> weather(@Url String url, @Query("cityname") String cityName);
+
+    @GET
+    Call<WeatherWrapper> weather(@Header("apikey") String apikey, @Url String url, @Query("cityname") String cityName);
+
+    @GET
+    Call<WeatherWrapper> weather(@HeaderMap Map<String, String> headers, @Url String url, @Query("cityname") String cityName);
+
 }
